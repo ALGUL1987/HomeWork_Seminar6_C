1953e3dfdfa01104e160573f16dee9dad51e4675
ALGUL1987 <tankipanki1@gmail.com>
2023-03-02 21:06:26 +0300

    finish

---
+++ 001/Program.cs
@@ -0,0 +1,34 @@
+﻿/*
+Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+0, 7, 8, -2, -2 -> 2
+
+1, -7, 567, 89, 223-> 3
+*/
+
+Console.WriteLine("Введите количество чисел: ");
+int size = Convert.ToInt32(Console.ReadLine());
+double min = new Random().Next();
+double max = new Random().Next();
+
+double[] array = GetArray(size, min, max);
+double[] GetArray(int size, double minValue, double maxValue)
+{
+    double[] result = new double[size];
+    for (int i = 0; i < size; i++)
+    {
+        Console.WriteLine("Введите число: ");
+        result[i] = Convert.ToDouble(Console.ReadLine());
+    }
+    return result;
+}
+int count=0;
+for (int i = 0; i < size; i++)
+{
+    if (array[i]>0)
+    {
+        count++;
+    }
+}
+Console.WriteLine($"Среди введенных чисел: {String.Join("; ", array)} чисел >0 -> {count}");
+
---
+++ 003/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+Задача 42 (необязательно): Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+45 -> 101101
+3 -> 11
+2 -> 10
+*/
+
+Console.WriteLine("Введите число: ");
+int value = Convert.ToInt32(Console.ReadLine());
+int i = 0;
+int valueCount = value;
+int valueArray = value;
+while (valueCount > 0)
+{
+    valueCount = valueCount / 2;
+    i++;
+}
+int[] binaryValue = new int[i];
+int size = i;
+for (int j = 0; j < size; j++)
+{
+    binaryValue[size-1-j] = valueArray % 2;
+    valueArray = valueArray / 2;
+    i++;
+}
+Console.WriteLine($" Число {value} в двоичном коде {String.Join(" ", binaryValue)}");
+
+
+
+
+
